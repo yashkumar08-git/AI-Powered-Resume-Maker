73de9d84447e58cb6ddb30de93324361467c4583
Yash Porwal <yporwal907@gmail.com>
2025-08-22 19:31:10 +0000

    remove the color from the box

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm backdrop-blur-md",
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
