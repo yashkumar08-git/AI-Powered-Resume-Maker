820db8a1a6d9bfd52cc0e8046acf1b3e83ad7c38
Yash Porwal <yporwal907@gmail.com>
2025-08-23 07:43:04 +0000

    now also it is moving

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,10 @@ export default function RootLayout({
       <body
         id="main-body"
         className={cn(
-          'min-h-screen bg-background font-body antialiased flex flex-col',
-          'bg-image'
+          'min-h-screen bg-background font-body antialiased flex flex-col'
         )}
       >
+        <div className="fixed inset-0 -z-10 bg-image-container" />
         <AuthProvider>
           <div className="relative flex flex-col min-h-screen bg-background/60">
             <Header />
